407282fb5e77cb51b0ad8de3aee8c99a00634c59
Jan Häusler <jan.haeusler@rwth-aachen.de>
2022-02-02 14:45:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,144 +16,10 @@ const prop = (name) => {
 }
 
 const objects = {
-  ventilSet: { type: 1, instance: 1207},
-  ventilGet: { type: 0, instance: 1208},
+  ventilSet: { type: 1, instance: 1207 },
+  ventilGet: { type: 0, instance: 1208 },
   jalouise: { type: 5, instance: 3204002 }
 }
-
-const orig = [
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": 0,
-    "type": 9
-  },
-  {
-    "value": null,
-    "type": 0
-  }
-];
-
-const prep = [
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": 1,
-    "type": 9
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": null,
-    "type": 0
-  },
-  {
-    "value": 0,
-    "type": 9
-  },
-  {
-    "value": null,
-    "type": 0
-  }
-]
 
 // Read Device Object
 const requestArray = [{
@@ -161,10 +27,10 @@ const requestArray = [{
   properties: [prop('priorityArray'), prop('objectName')]
 }];
 client.readPropertyMultiple('192.168.200.34', requestArray, (err, value) => {
-  console.log('value: ', JSON.stringify(value, null, 2));
+  console.log('value:', JSON.stringify(value, null, 2));
 });
-client.writeProperty('192.168.200.34', objects.jalouise, propertyIds.presentValue, [{type: 9, value: 1}], { priority: 16 }, (err) => {
-  console.log('error: ', err);
+client.writeProperty('192.168.200.34', objects.jalouise, propertyIds.presentValue, [{ type: 9, value: 1}], { priority: 14 }, (err) => {
+  console.log('error:', err);
 });
 
 // for(let i = 81; i < 123; i++)
