73eb2f566e1c5478e48f81023ad67104730b8dd2
janhaa <jan.haeusler@rwth-aachen.de>
2023-06-05 12:13:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const bacnet = require('bacstack');
-const { argv } = process;
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const argv = yargs(hideBin(process.argv)).argv;
